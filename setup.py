0da606279a015b6154fa5ad902bdff41a43c82b6
NinaadRao <ninaadrrao@gmail.com>
2021-04-11 10:47:18 +0530

    remove package_dir, pymodules

--- setup.py
+++ setup.py
@@ -34,21 +34,20 @@ class Install(install):
 
 setup(
     name='pyhumour',
-    version='0.0.8',
+    version='0.1.0',
     description='A module for the characterization and quantification of concise humour',
     license='BSD-3-Clause',
-    packages=find_packages('pyhumour'),
-    package_dir={'': 'pyhumour'},
+    # package_dir={'': 'pyhumour'},
+    packages=find_packages(),
     # py_modules=[splitext(basename(path))[0] for path in glob('pyhumour/*.py')],
-    py_modules=["pyhumour"],  # list of files that can be imported
-    package_data={'pyhumour': ['resources/*.json', 'resources/*.txt']},
+    # py_modules=["pyhumour/*"],  # list of files that can be imported
+    # package_data={'pyhumour': ['resources/*.json', 'resources/*.txt']},
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent"
     ],
+    python_requires=">=3.6",
     include_package_data=True,
     zip_safe=False,
     long_description=long_description,
@@ -63,8 +62,8 @@ setup(
         "wordfreq>=2.2.2",
         "hmmlearn>=0.2.3",
         "nltk>=3.3",
-        "Tensorflow >= 2.2",
-        "Keras>=2.3.1",
+        "tensorflow>=2.2",
+        "keras>=2.3.1",
     ],
     extras_require={
         "dev": [
